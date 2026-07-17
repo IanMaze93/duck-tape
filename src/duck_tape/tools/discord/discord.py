dd929d389dec94f7dcea590bcb3c1cc5de808944
Ian Maze <mazeiandev@outlook.com>
2026-07-16 21:01:48 -0400

    feat(cli): [DT-13] update discord command

--- src/duck_tape/tools/discord/discord.py
+++ src/duck_tape/tools/discord/discord.py
@@ -6,4 +6,10 @@ def launch_discord():
     Starts Discord.
     """
 
-    subprocess.Popen(["discord"])
+    subprocess.Popen(
+        ["discord"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        stdin=subprocess.DEVNULL,
+        start_new_session=True,
+    )
